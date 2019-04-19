931848c1af963a04806b7bc0b3e6c7ef2a21fe7e
Marc Wouts <marc.wouts@gmail.com>
2019-04-20 00:47:16 +0200

    init_itable_mode shows all df

--- itables/__init__.py
+++ itables/__init__.py
@@ -1,6 +1,6 @@
-from .javascript import show, load_datatables, init_itables
+from .javascript import show, load_datatables, init_itable_mode
 from .version import __version__
 
 load_datatables()
 
-__all__ = ['__version__', 'show', 'init_itables']
+__all__ = ['__version__', 'show', 'init_itable_mode']
