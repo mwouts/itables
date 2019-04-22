4544f527c15ffd3b60cf0dda7edb656376bb8b8f
Marc Wouts <marc.wouts@gmail.com>
2019-04-23 01:16:50 +0200

    pylint

--- itables/options.py
+++ itables/options.py
@@ -1,6 +1,6 @@
 """Global options for the Interactive Tables"""
 
-"""Show the index? Possible values: True, False and 'auto'. In mode 'auto', the index is not shown 
+"""Show the index? Possible values: True, False and 'auto'. In mode 'auto', the index is not shown
 if it has no name and its content is range(N)"""
 showIndex = 'auto'
 
--- itables/sample.py
+++ itables/sample.py
@@ -1,10 +1,13 @@
+"""A few sample data frames, useful for testing"""
+
 import math
+from datetime import datetime, timedelta
 import pandas as pd
 import numpy as np
-from datetime import datetime, timedelta
 
 
 def sample_dfs():
+    """A list of dataframes with various dtypes"""
     return [pd.DataFrame({'a': [1, 2, 3], 'b': [4, 5, 6]}),
             pd.DataFrame({'a': [0, np.Infinity, np.NaN], 'b': [-1.0, -np.Infinity, math.pi]}),
             pd.DataFrame({'col.1': ['a', 'b'], 'col.2': ['c', 'd']}),
@@ -18,6 +21,7 @@ def sample_dfs():
 
 
 def sample_series():
+    """A list of series with various dtypes"""
     series = []
     for df in sample_dfs():
         for col in df:
