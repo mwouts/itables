f8ee0b8fe22d7b4334d43792283dfac7d25454b3
Marc Wouts <marc.wouts@gmail.com>
2019-04-23 01:19:36 +0200

    Simplify tests

--- tests/test_notebooks.py
+++ tests/test_notebooks.py
@@ -1,10 +1,12 @@
+"""Test that the code in all the test notebooks work, including README.md"""
+
 import os
 import pytest
 import jupytext
-from nbconvert.preprocessors import ExecutePreprocessor
 
 
 def list_notebooks():
+    """List the md notebooks in this package"""
     nb_path = os.path.dirname(os.path.abspath(__file__))
     notebooks = [os.path.join(nb_path, nb_file) for nb_file in os.listdir(nb_path) if nb_file.endswith('.md')]
     notebooks.append(os.path.join(nb_path, '..', 'README.md'))
@@ -15,6 +17,9 @@ def list_notebooks():
 
 @pytest.mark.parametrize('md_file', list_notebooks())
 def test_run_notebooks(md_file):
-    nb = jupytext.readf(md_file)
-    ep = ExecutePreprocessor(timeout=None, kernel_name='current_env')
-    ep.preprocess(nb, {'metadata': {'path': os.path.dirname(os.path.abspath(__file__))}})
+    """Execute each notebook"""
+    notebook = jupytext.readf(md_file)
+    context = {}
+    for cell in notebook.cells:
+        if cell.cell_type == 'code':
+            exec(cell.source, context)
