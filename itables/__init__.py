ee4e3c05392107f8940540b503817b47678dadf6
Marc Wouts <marc.wouts@gmail.com>
2020-12-10 23:50:06 +0100

    New function init_notebook_mode

--- itables/__init__.py
+++ itables/__init__.py
@@ -1,6 +1,4 @@
-from .javascript import show, load_datatables
+from .javascript import show, init_notebook_mode
 from .version import __version__
 
-load_datatables()
-
-__all__ = ['__version__', 'show']
+__all__ = ['__version__', 'show', 'init_notebook_mode']
--- itables/version.py
+++ itables/version.py
@@ -1,3 +1,3 @@
 """ITables' version number"""
 
-__version__ = '0.2.2'
+__version__ = '0.3.0'
