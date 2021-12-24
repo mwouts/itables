7fe3507558bc775b1b18aa0ae7c1632602d0f2d0
Marc Wouts <marc.wouts@gmail.com>
2021-12-24 12:11:51 +0100

    Require 'jquery' and upgrade jquery and datatables

--- itables/javascript/load_datatables_connected.js
+++ itables/javascript/load_datatables_connected.js
@@ -1,6 +1,7 @@
 require.config({
     paths: {
-        datatables: 'https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min',
+        jquery: 'https://code.jquery.com/jquery-3.5.1.min',
+        datatables: 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min',
     }
 });
 
