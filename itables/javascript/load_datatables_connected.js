9b2c446f0324c4bcb9c018f20281ec8f5f0c1549
Marc Wouts <marc.wouts@gmail.com>
2022-01-05 10:11:32 +0100

    Set the table style locally

--- itables/javascript/load_datatables_connected.js
+++ itables/javascript/load_datatables_connected.js
@@ -4,9 +4,3 @@ require.config({
         datatables: 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min',
     }
 });
-
-require(['jquery'], function($) {
-    $('head').append('<link rel="stylesheet" type="text/css" \
-                href = "https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" > ');
-    $('head').append('<style> table td { text-overflow: ellipsis; overflow: hidden; } </style>');
-});
