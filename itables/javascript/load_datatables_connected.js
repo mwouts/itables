bc865bce1806096b2163c70cb589818ba0a38b17
Marc Wouts <marc.wouts@gmail.com>
2022-01-06 18:01:32 +0100

    Load datatables.net with an ES import when `require.js` is not available Many thanks to @fwouts for his precious help on this!

--- itables/javascript/load_datatables_connected.js
+++ itables/javascript/load_datatables_connected.js
@@ -1,6 +1,7 @@
-require.config({
-    paths: {
-        jquery: 'https://code.jquery.com/jquery-3.5.1.min',
-        datatables: 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min',
-    }
-});
+if (typeof require !== 'undefined')
+    require.config({
+        paths: {
+            jquery: 'https://code.jquery.com/jquery-3.5.1.min',
+            datatables: 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min',
+        }
+    });
