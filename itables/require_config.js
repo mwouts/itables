7b1aa5ec4aa9b5f1856b1229f17a49332c01451b
Marc Wouts <marc.wouts@gmail.com>
2022-05-04 01:10:15 +0200

    load the jQuery and datatables libraries in the document head and drop both import and requirejs

--- itables/require_config.js
+++ itables/require_config.js
@@ -2,6 +2,6 @@ if (typeof require !== 'undefined')
     require.config({
         paths: {
             jquery: 'https://code.jquery.com/jquery-3.5.1.min',
-            datatables: 'https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min',
+            datatables: 'https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min',
         }
     });
