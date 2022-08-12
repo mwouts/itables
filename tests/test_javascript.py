f26796114fd03d9dba90168ec4842a627f623f5d
Marc Wouts <marc.wouts@gmail.com>
2022-08-12 23:48:40 +0200

    New function 'to_html_datatable'

--- tests/test_javascript.py
+++ tests/test_javascript.py
@@ -3,8 +3,8 @@ import pandas as pd
 from itables import javascript
 
 
-def test_datatables_repr_max_columns_none():
+def test_to_html_datatable_max_columns_none():
     test_df = pd.DataFrame([1, 2])
     with pd.option_context("display.max_columns", None):
-        html = javascript._datatables_repr_(test_df)
+        html = javascript.to_html_datatable(test_df)
         assert html
