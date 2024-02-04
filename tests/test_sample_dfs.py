64f7b04a26d30d1a92f2568a7eb0c5f9ef55c389
Marc Wouts <marc.wouts@gmail.com>
2024-02-04 17:46:01 +0000

    Document ITables in Quarto Add data-quarto-disable-processing="true" to the tables that are not generated with to_html

--- tests/test_sample_dfs.py
+++ tests/test_sample_dfs.py
@@ -85,12 +85,16 @@ def test_show_test_dfs(df, connected, use_to_html, lengthMenu, monkeypatch):
 def test_to_html_datatable(df, connected, use_to_html, lengthMenu, monkeypatch):
     if "bigint" in df.columns:
         monkeypatch.setattr("itables.options.warn_on_int_to_str_conversion", False)
-    to_html_datatable(
+    html = to_html_datatable(
         df,
         connected=connected,
         use_to_html=use_to_html,
         **kwargs_remove_none(lengthMenu=lengthMenu)
     )
+    if use_to_html:
+        assert "quarto" not in html
+    else:
+        assert 'data-quarto-disable-processing="true"' in html
 
 
 def test_ordered_categories():
