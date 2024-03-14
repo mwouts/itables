9260b16bfb4eb82a24389c202f7a16bb11584a02
Marc Wouts <marc.wouts@gmail.com>
2024-03-14 21:08:28 +0000

    The default dt_url works for Shiny apps too

--- itables/shiny.py
+++ itables/shiny.py
@@ -2,19 +2,7 @@ from .javascript import to_html_datatable
 
 
 def DT(df, caption=None, tableId=None, **kwargs):
-    """This is a version of 'to_html_datatable' that works in Shiny applications.
-
-    In these applications, jquery is already loaded, so we call 'to_html_datatable'
-    with a custom dt_url that does not contain jQuery.
-
-    Cf. https://github.com/mwouts/itables/issues/181
-    and https://github.com/rstudio/py-shiny/issues/502
-    """
+    """This is a version of 'to_html_datatable' that works in Shiny applications."""
     return to_html_datatable(
-        df,
-        caption=caption,
-        tableId=tableId,
-        connected=True,
-        dt_url="https://cdn.datatables.net/v/dt/dt-2.0.2/datatables.min.js",
-        **kwargs
+        df, caption=caption, tableId=tableId, connected=True, **kwargs
     )
