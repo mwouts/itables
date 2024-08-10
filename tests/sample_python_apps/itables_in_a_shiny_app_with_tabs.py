4c7548e315fc054c39829378002c85fd04763e1d
Marc Wouts <marc.wouts@gmail.com>
2024-08-10 20:55:32 +0100

    Fix the demo shiny app with tabs

--- tests/sample_python_apps/itables_in_a_shiny_app_with_tabs.py
+++ tests/sample_python_apps/itables_in_a_shiny_app_with_tabs.py
@@ -1,14 +1,20 @@
+# Run this app with "shiny run file.py"
 from shiny import App, ui
 
 from itables.sample_dfs import get_dict_of_test_dfs
 from itables.shiny import DT
 
+try:
+    # This one is not available on the CI (Python 3.8)
+    ui_nav = ui.nav
+except AttributeError:
+    ui_nav = ui.nav_panel
+
 app_ui = ui.page_fluid(
     # Display the different tables in different tabs
     ui.navset_tab(
-        *[ui.nav(name, ui.HTML(DT(df))) for name, df in get_dict_of_test_dfs().items()]
+        *[ui_nav(name, ui.HTML(DT(df))) for name, df in get_dict_of_test_dfs().items()]
     )
 )
 
 app = App(app_ui, server=None)
-# app.run()
