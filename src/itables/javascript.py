196343b985bafaac16fe9914c1c349d77dcf072c
Marc Wouts <marc.wouts@gmail.com>
2025-04-27 15:57:13 +0100

    Fix find_spec(google.colab)

--- src/itables/javascript.py
+++ src/itables/javascript.py
@@ -69,7 +69,7 @@ DEFAULT_LAYOUT = {
 DEFAULT_LAYOUT_CONTROLS = set(DEFAULT_LAYOUT.values())
 
 
-GOOGLE_COLAB = find_spec("google.colab") is not None
+GOOGLE_COLAB = find_spec("google") and (find_spec("google.colab") is not None)
 
 
 def init_notebook_mode(
