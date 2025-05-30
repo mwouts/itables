ec526acfa8fa15aaab28e6911a1acdbb37f0cdd4
Marc Wouts <marc.wouts@gmail.com>
2025-05-30 22:23:25 +0100

    More Marimo examples

---
+++ apps/marimo/column_visibility.py
@@ -0,0 +1,35 @@
+# /// script
+# requires-python = ">=3.12,<3.13"
+# dependencies = [
+#     "anywidget==0.9.18",
+#     "itables==2.4.0",
+#     "marimo",
+#     "pandas==2.2.3",
+# ]
+# ///
+
+import marimo
+
+__generated_with = "0.13.6"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import pandas as pd
+
+    from itables.widget import ITable
+
+    df = pd.DataFrame({"x": [2, 1, 3], "y": list("cbc")})
+
+    ITable(df, buttons=["pageLength", "copyHtml5", "csvHtml5", "excelHtml5", "colvis"])
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
