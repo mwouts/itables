067ba09d0a27c8256e269df5c5f115994a79e8b0
Marc Wouts <marc.wouts@gmail.com>
2026-06-15 16:29:02 +0200

    Transition to Jupyter Book v2

--- docs/py/options/col_reorder.py
+++ docs/py/options/col_reorder.py
@@ -17,7 +17,8 @@
 # %% [markdown]
 # # Col Reorder
 #
-# ```{versionadded} 2.8.1
+# ```{note}
+# New in version 2.8.1
 # ```
 #
 # [ColReorder](https://datatables.net/extensions/colreorder/) is a DataTables extension
